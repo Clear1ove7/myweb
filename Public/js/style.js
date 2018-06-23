8eb2b8c1d352566140a531865ecc2ae8d7081b92
Clear1ove7 <727617616@qq.com>
2018-06-23 17:02:06 +0800

    网站首页搭建

--- Public/js/style.js
+++ Public/js/style.js
@@ -1,7 +1,9 @@
 $(function(){
+	//根据f+数字的class来设置字体大小
 	for(var i=10;i<=50;i++){
 		$('.f'+i).css('font-size',i+'px');
 	}
+	//line-height自适应
 	for(var i=0;i<$('.lineH').length;i++){
 		$('.lineH')?$('.lineH:eq('+i+')').css('line-height',$('.lineH:eq('+i+')').height()+'px'):'';
 	}
--- Web/Runtime/Cache/Home/a5d07c113795da31d2426caff16ba5bc.php
+++ Web/Runtime/Cache/Home/a5d07c113795da31d2426caff16ba5bc.php
@@ -1,24 +1,47 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html lang="zh-CN">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="Public/boot/css/bootstrap.min.css" />
 <script type="text/javascript" src="Public/js/jquery.min.js"></script>
 <script type="text/javascript" src="Public/boot/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="Public/js/style.js"></script>
+<script type="text/javascript" src="Public/js/jquery.mobile-1.4.5.js"></script>
 <link rel="stylesheet" href="Public/css/style.css" />
-		<title></title>
-	</head>
-	<body class="cont inBl">
-		<header class="after_bot lineH">
-	<h1 class="titles f30"><?php echo ($_SESSION['user']['name']); ?>您好！欢迎来到首页</h1>
-	<?php if($_SESSION['user']['name']): ?><a class="bg_blues btn_login lineH" href="<?php echo u('Login/logout');?>">退出</a>
-	<?php else: ?> 
-		<a class="bg_blues btn_login lineH" href="<?php echo u('Login/login');?>">登陆</a><?php endif; ?>
-	
-</header>
-
-	</body>
+	<link href="Public/css/index.css" rel="stylesheet">
+	<title>---</title>
+</head>
+<body>
+	<div class="main">
+		<div class="items item1"></div>
+		<div class="items item2"></div>
+		<div class="items item3"></div>
+		<div class="items item4"></div>
+		<div class="items item5"></div>
+	</div>
+	<div class="rigTap">
+		<div class="taps active" data-num = 0 ><span class="glyphicon glyphicon-circle-arrow-up f14"></span></div>
+		<div class="taps" data-num = 1 ><span class="glyphicon glyphicon-circle-arrow-down f14"></span></div>
+		<div class="taps" data-num = 2 ><span class="glyphicon glyphicon-plus-sign f14"></span></div>
+		<div class="taps" data-num = 3 ><span class="glyphicon glyphicon-circle-arrow-left f14"></span></div>
+		<div class="taps" data-num = 4 ><span class="glyphicon glyphicon-circle-arrow-right f14"></span></div>
+	</div>
+	<script src="Public/js/index.js"></script> 
+	<script>
+		//判断是否为pc端
+		function is_pc(){  
+            var userAgentInfo = navigator.userAgent;  
+            var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
+            var flag = true;  
+            for (var v = 0; v < Agents.length; v++) {  
+               if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }  
+            }  
+           return flag;  
+		}
+		//
+		is_pc()?index.scroll():index.touchFun();
+		index.tapFun();
+	</script>
+</body>
 </html>
